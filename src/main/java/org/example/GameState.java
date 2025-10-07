900c950320940c065db5bfe7f0c95187e9502998
Muhammad <asadullahnadeem48@gmail.com>
2025-10-07 14:57:52 +0530

    update and Add Code

--- src/main/java/org/example/GameState.java
+++ src/main/java/org/example/GameState.java
@@ -1,12 +1,10 @@
 package org.example;
 
-/**
- * Represents the different states or screens of the game.
- */
 public enum GameState {
     MENU,
     BALL_SELECTION,
     PLAYING,
     ABOUT,
-    GAME_OVER
+    GAME_OVER,
+    PAUSED
 }
